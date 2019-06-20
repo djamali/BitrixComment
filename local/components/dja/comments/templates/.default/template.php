b62fe4790a3e12dc16ffeaf020e2a52898f9c14d
djamali <dolakovo@mail.ru>
2019-06-20 14:47:57 +0300

    Update template.php

--- local/components/dja/comments/templates/.default/template.php
+++ local/components/dja/comments/templates/.default/template.php
@@ -1,6 +1,6 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <form method="post" action="">
-Добавить комментарий<br>
+Р’РІРµРґРёС‚Рµ РєРѕРјРјРµРЅС‚Р°СЂРёР№<br>
 <textarea rows="4" cols="55" name="text"></textarea><br>
 <input type="submit">
 </form>
